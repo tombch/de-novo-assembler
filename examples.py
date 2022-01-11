7a1f2c493f51ce824d80611d86ce618976970ca9
tombch <tombrier@hotmail.co.uk>
2022-01-11 12:36:27 +0000

    current examples.py

--- examples.py
+++ examples.py
@@ -1,58 +1,61 @@
 import assembler as ar
-import random
 from timeit import default_timer as timer
 
 
 def main():
-    # Example 1
-    message = 'a_galaxy_far__far___far____far_____away...'
-    kmers = ar.get_kmers(message, 10)
-    random.shuffle(kmers)
+    # Example 0
     start = timer()
-    assembled_message = ar.assemble(kmers, describe=True)
+    sequence = ar.generate_sequence(30000)
+    kmers = ar.get_kmers(sequence, k=100)
+    graph = ar.de_bruijn_graph(kmers)
+    path = ar.eulerian_path(graph)
+    assembled_sequence = ar.join(path)
+    dist = ar.hamming_distance(sequence, assembled_sequence)
     end = timer()
-    print(f'time taken: {end - start}')
-    distance = ar.hamming_distance(message, assembled_message)
-    print(f'dist(original, assembled) = {distance}')
-    
-    print('')
+    print(f'dist(original, assembled) = {dist}')
+    print(f'Time taken: {end - start} seconds')
+
+    # Example 1
+    # message = 'a_galaxy_far_faar_far_far_away...'
+    # kmers = ar.get_kmers(message, 5)
+    # kmers = ar.noisify(kmers)
+    # print(ar.de_bruijn_graph(kmers))
+    # assembled_message = ar.assemble(kmers, describe=True)
+    # distance = ar.hamming_distance(message, assembled_message)
+    # print(f'dist(original, assembled) = {distance}')
+    # print(message)
+    # print(assembled_message)
 
     # Example 2
-    sequence = ar.generate_sequence(50000)
-    coverage = 100
-    kmers = []
-    # Simulate noisy kmers
-    for i in range(coverage):
-        kmers += ar.mutate(ar.get_kmers(sequence, 50), num_mutations=3000)
-    random.shuffle(kmers)
-    ar.kmer_count_distribution(kmers, threshold=10, plot=True)
-    start = timer()
-    kmers = ar.estimate_coverage_reduce_kmers(kmers, threshold=10, describe=True)
-    assembled_sequence = ar.assemble(kmers, describe=True)
-    end = timer()
-    print(f'time taken: {end - start}')
-    distance = ar.hamming_distance(sequence, assembled_sequence)
-    print(f'dist(original, assembled) = {distance}')
-    
-    print('')
+    # sequence = ar.generate_sequence(50000, bases='atcg')
+    # kmers = []
+    # # Simulate high coverage
+    # kmers = ar.get_kmers(sequence, k=50, coverage=100)
+    # # Introduce noise to kmers
+    # kmers = ar.noisify(kmers, kmer_mutations=100000, kmer_deletions=100000)
+    # # Show distribution of kmer counts to indicate coverage
+    # # ar.kmer_count_distribution(kmers, threshold=10, plot=True)
+    # # Assemble sequence
+    # assembled_sequence = ar.assemble(kmers, describe=True, threshold=10)
+    # # Find string distance
+    # distance = ar.hamming_distance(sequence, assembled_sequence)
+    # print(f'dist(original, assembled) = {distance}')
 
     # Example 3
-    with open("genome.txt") as genome_fh:
-        genome = genome_fh.readline()
-        clean_kmers = ar.get_kmers(genome, 300)
-        kmers = []
-        coverage = 100
-        for i in range(coverage):
-            kmers += ar.mutate(clean_kmers, num_mutations=3000)
-        random.shuffle(kmers)
-        ar.kmer_count_distribution(kmers, threshold=10, plot=True)
-        start = timer()
-        kmers = ar.estimate_coverage_reduce_kmers(kmers, threshold=10, describe=True)
-        assembled_genome = ar.assemble(kmers, describe=True)
-        end = timer()
-        print(f'time taken: {end - start}')
-        distance = ar.hamming_distance(genome, assembled_genome)
-        print(f'dist(original, assembled) = {distance}')
+    # with open("genome.txt") as genome_fh:
+    #     genome = genome_fh.readline()
+    #     kmers = []
+    #     # Simulate high coverage
+    #     kmers = ar.get_kmers(genome, k=300, coverage=100)
+    #     # Introduce noise to kmers
+    #     kmers = ar.noisify(kmers, kmer_mutations=100000, kmer_deletions=100000)
+    #     # Show distribution of kmer counts to indicate coverage
+    #     # ar.kmer_count_distribution(kmers, threshold=10, plot=True)
+    #     # Assemble sequence
+    #     assembled_genome = ar.assemble(kmers, describe=True, threshold=10)
+    #     # Find string distance
+    #     distance = ar.hamming_distance(genome, assembled_genome)
+    #     print(f'dist(original, assembled) = {distance}')
 
 
 if __name__ == '__main__':
